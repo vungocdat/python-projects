937012bdcf0c45c9d89b1913a42222805329061a
Vu Ngoc Dat <ngoc.dat.vu@seznam.cz>
2022-03-09 14:42:13 +0100

    created Snake class

--- Snake-game/main.py
+++ Snake-game/main.py
@@ -2,33 +2,18 @@
 
 import turtle as t
 import time
+from snake import Snake
 
 screen = t.Screen()
 screen.title("Retro snake game")
 
-snake = []
-
-# Create starting snake with size of 3 squares
-starting_body = 3
-for i in range(starting_body):
-    snake_body = t.Turtle(shape="square")
-    snake_body.penup()  # to not draw a line while moving
-    snake_body.setx(i * -20)
-    snake.append(snake_body)
+snake = Snake()
 
 screen.tracer(0)
 snake_alive = True
 while snake_alive:
     screen.update()  # update the screen -> shows the snake
     time.sleep(0.1)
-    # start moving the last part of snake and move it to the position, where the previous part was
-    for i in range(len(snake) - 1, 0, -1):
-        previous_x = snake[i - 1].xcor()
-        previous_y = snake[i - 1].ycor()
-        snake[i].goto(previous_x, previous_y)
-
-    snake[0].forward(20)
-    snake[0].left(90)
 
 
 
